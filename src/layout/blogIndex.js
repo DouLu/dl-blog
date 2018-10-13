09a60684c33b0b7ebc7ed95eef370129d83512ea
doulu <wb-dl449432@alibaba-inc.com>
2018-10-13 18:55:47 +0800

    feat(布局)：博客首页layout

--- src/layout/blogIndex.js
+++ src/layout/blogIndex.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Menu, Icon } from 'antd';
 import IndexRouter from '../router';
+import './style.scss';
 
 const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
 
 export default class IndexLayout extends Component {
   constructor(props) {
@@ -25,57 +25,45 @@ export default class IndexLayout extends Component {
     return (
       <Router>
         <div>
-          <Menu
-            onClick={this.handleClick}
-            selectedKeys={[this.state.current]}
-            mode="horizontal"
-          >
-            <Menu.Item key="mail">
-              <Icon type="mail" />Navigation One
-        </Menu.Item>
-            <Menu.Item key="app" disabled>
-              <Icon type="appstore" />Navigation Two
-        </Menu.Item>
-            <SubMenu title={<span className="submenu-title-wrapper"><Icon type="setting" />Navigation Three - Submenu</span>}>
-              <MenuItemGroup title="Item 1">
-                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                <Menu.Item key="setting:2">Option 2</Menu.Item>
-              </MenuItemGroup>
-              <MenuItemGroup title="Item 2">
-                <Menu.Item key="setting:3">Option 3</Menu.Item>
-                <Menu.Item key="setting:4">Option 4</Menu.Item>
-              </MenuItemGroup>
-            </SubMenu>
-            <Menu.Item key="alipay">
-              <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
-            </Menu.Item>
-          </Menu>
           <div className="main-menu">
-            <ul>
-              <li>
-                <Link to="/blog/index">首页</Link>
-              </li>
-              <li>
-                分类
-                <ul>
-                  <li>
-                    <Link to="/blog/category?id=jishu">技术</Link>
-                  </li>
-                  <li>
-                    <Link to="/blog/category?id=living">人生</Link>
-                  </li>
-                  <li>
-                    <Link to="/blog/category/feel">感动</Link>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <Link to="/blog/tuya">涂鸦</Link>
-              </li>
-              <li>
-                <Link to="/blog/shudong">树洞</Link>
-              </li>
-            </ul>
+            <Menu
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+            >
+              <Menu.Item key="index">
+                <Link to="/blog/index">
+                  <Icon type="home" />首页
+                </Link>
+              </Menu.Item>
+              <SubMenu title={<span className="submenu-title-wrapper"><Icon type="setting" />分类</span>}>
+                <Menu.Item key="jishu">
+                  <Link to="/blog/category?id=jishu">
+                    <Icon type="github" /> 技术
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="living">
+                  <Link to="/blog/category?id=living">
+                    <Icon type="twitter" /> 人生
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="feel">
+                  <Link to="/blog/category/feel">
+                    <Icon type="gitlab" /> 感动
+                  </Link>
+                </Menu.Item>
+              </SubMenu>
+              <Menu.Item key="tuya">
+                <Link to="/blog/tuya">
+                  <Icon type="yuque" />涂鸦
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="shudong">
+                <Link to="/blog/shudong">
+                  <Icon type="chrome" />树洞
+                </Link>
+              </Menu.Item>
+            </Menu>
           </div>
           <div className="container">
             <div className="content">
@@ -83,7 +71,7 @@ export default class IndexLayout extends Component {
             </div>
             <div className="r-aside">
               右侧登录注册组件
-          </div>
+            </div>
           </div>
         </div>
       </Router>
