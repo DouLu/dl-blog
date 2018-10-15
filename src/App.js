f040939e0e3e9e4916fd3cbcd0172d9bbfd5ef9d
doulu <wb-dl449432@alibaba-inc.com>
2018-10-15 21:53:00 +0800

    feat(后台布局):添加后台入口

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import IndexLayout from '../src/layout/blogIndex';
+import AdminIndex from './layout/adminIndex';
 class App extends Component {
   render() {
     return (
-      <IndexLayout />
+      <Router>
+        <Switch>
+          <Route path="/admin" component={AdminIndex} />
+          <Route path="/" component={IndexLayout} />
+        </Switch>
+      </Router>
     );
   }
 }
